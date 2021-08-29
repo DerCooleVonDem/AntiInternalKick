ee48b09b3ebc3473b7dbe4e3298a047d73e697a0
jibixyt <timjeffga@gmail.com>
2021-08-29 17:35:11 +0200

    Initial commit

---
+++ src/Jibix/AntiInternalKick/Main.php
@@ -0,0 +1,15 @@
+<?php
+namespace Jibix\AntiInternalKick;
+use pocketmine\plugin\PluginBase;
+
+
+/**
+ * Class Main
+ * @package Jibix\AntiInternalKick
+ * @author Jibix
+ * @date 29.08.2021 - 17:25
+ * @project AntiInternalKick
+ */
+class Main extends PluginBase{
+
+}
